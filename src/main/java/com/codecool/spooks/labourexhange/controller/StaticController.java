1bfb56f8d166b95d8f5261a05a89ac3ac897dfa4
VM-Codecool <kiflike6@gmail.com>
2018-03-05 16:04:00 +0100

    contollerAddedForStatic

--- src/main/java/com/codecool/spooks/labourexhange/controller/StaticController.java
+++ src/main/java/com/codecool/spooks/labourexhange/controller/StaticController.java
@@ -1,9 +1,5 @@
 package com.codecool.spooks.labourexhange.controller;
 
-import com.codecool.spooks.labourexhange.model.adverts.Advertisement;
-import com.codecool.spooks.labourexhange.model.adverts.Status;
-import com.codecool.spooks.labourexhange.service.AdvertisementService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
