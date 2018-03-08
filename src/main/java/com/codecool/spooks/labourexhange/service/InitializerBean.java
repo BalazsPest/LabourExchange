28aeb052056768949436d1bada45a256879f1414
simofanna <simofanna93@gmail.com>
2018-03-08 13:54:11 +0100

    add data + companyindex fix

--- src/main/java/com/codecool/spooks/labourexhange/service/InitializerBean.java
+++ src/main/java/com/codecool/spooks/labourexhange/service/InitializerBean.java
@@ -24,6 +24,10 @@ public class InitializerBean {
         //Adding languages
         languageService.addNewLanguage("german", Language.LanguageLevel.BASIC);
         languageService.addNewLanguage("english", Language.LanguageLevel.HIGH);
+        languageService.addNewLanguage("spanish", Language.LanguageLevel.BASIC);
+        languageService.addNewLanguage("romanian", Language.LanguageLevel.HIGH);
+        languageService.addNewLanguage("italian", Language.LanguageLevel.BASIC);
+
         //Adding cities
 
         cityService.addNewCity("Miskolc");
@@ -35,8 +39,38 @@ public class InitializerBean {
         //Adding students
         studentService.addStudent("Molnár Árpád", "arpi@haho.hu", "arpi", "haha", Student.Gender.MALE,
                                   "2000.02.25.", cityService.getCityByName("Budapest"), Arrays.asList(languageService.getLanguageByName("english")));
+        studentService.addStudent("Kiss János", "janos@gmail.com", "arpi", "haha", Student.Gender.MALE,
+                "2000.02.25.", cityService.getCityByName("Budapest"), Arrays.asList(languageService.getLanguageByName("english")));
+        studentService.addStudent("Horváth Ákos", "akos@gmail.com", "arpi", "haha", Student.Gender.MALE,
+                "2000.02.25.", cityService.getCityByName("Budapest"), Arrays.asList(languageService.getLanguageByName("english")));
+        studentService.addStudent("Kiss Petra", "Petra@gmail.com", "arpi", "haha", Student.Gender.MALE,
+                "2000.02.25.", cityService.getCityByName("Budapest"), Arrays.asList(languageService.getLanguageByName("english")));
+        studentService.addStudent("Ferenci Dániel", "dani@gmail.com", "arpi", "haha", Student.Gender.MALE,
+                "2000.02.25.", cityService.getCityByName("Budapest"), Arrays.asList(languageService.getLanguageByName("english")));
+        studentService.addStudent("Zsellerecske Horváth Tamás", "tamás@gmail.com", "arpi", "haha", Student.Gender.MALE,
+                "2000.02.25.", cityService.getCityByName("Budapest"), Arrays.asList(languageService.getLanguageByName("english")));
+        studentService.addStudent("Piros Léna", "lenapiros@gmail.com", "arpi", "haha", Student.Gender.MALE,
+                "2000.02.25.", cityService.getCityByName("Budapest"), Arrays.asList(languageService.getLanguageByName("english")));
+        studentService.addStudent("Kiskőrösi Jázmin", "jazmin55@gmail.com", "arpi", "haha", Student.Gender.MALE,
+                "2000.02.25.", cityService.getCityByName("Budapest"), Arrays.asList(languageService.getLanguageByName("english")));
+
+
+
+
+
         //Adding companies
-         companyService.addCompany("procter", "procter@gmail.com", "proki", "proki");
+         companyService.addCompany("Procter", "procter@gmail.com", "proki", "proki");
+        companyService.addCompany("HBO", "procter@gmail.com", "proki", "proki");
+        companyService.addCompany("Concept EyeWear", "procter@gmail.com", "proki", "proki");
+        companyService.addCompany("YouTube", "procter@gmail.com", "proki", "proki");
+        companyService.addCompany("UPC", "procter@gmail.com", "proki", "proki");
+
+        companyService.addCompany("Google", "procter@gmail.com", "proki", "proki");
+        companyService.addCompany("Amazon", "procter@gmail.com", "proki", "proki");
+        companyService.addCompany("Prezi.com", "procter@gmail.com", "proki", "proki");
+        companyService.addCompany("Apple", "procter@gmail.com", "proki", "proki");
+        companyService.addCompany("General Electric", "procter@gmail.com", "proki", "proki");
+
         //Adding fields
         fieldService.addField("catering");
         fieldService.addField("housekeeping");
@@ -51,6 +85,14 @@ public class InitializerBean {
         advertisementService.addNewAdvert(studentService.getStudentByUsername("Molnár Árpád"), fieldService.getFieldByName("catering"), "Cheap diswashing", "I do everything", new Date(),
                 3, 500, cityService.getCityByName("Budapest"), Arrays.asList(tagService.getTagByName("waitressing"), tagService.getTagByName("cooking")));
 
-      
+        advertisementService.addNewAdvert(studentService.getStudentByUsername("Kiskőrösi Jázmin"), fieldService.getFieldByName("housekeeping"), "Nursing", "Im the best if you have a sick grandparent", new Date(),
+                3, 500, cityService.getCityByName("Budapest"), Arrays.asList(tagService.getTagByName("nursing"), tagService.getTagByName("nursing")));
+
+        advertisementService.addNewAdvert(studentService.getStudentByUsername("Zsellerecske Horváth Tamás"), fieldService.getFieldByName("it-related"), "Java Programmer", "Really good with the Java (un poco Python)", new Date(),
+                3, 500, cityService.getCityByName("Budapest"), Arrays.asList(tagService.getTagByName("nursing"), tagService.getTagByName("nursing")));
+
+        advertisementService.addNewAdvert(studentService.getStudentByUsername("Ferenci Dániel"), fieldService.getFieldByName("it-related"), "Frontend - developer", "Great frontend - bootstrap - html - thymeleaf", new Date(),
+                3, 500, cityService.getCityByName("Budapest"), Arrays.asList(tagService.getTagByName("nursing"), tagService.getTagByName("nursing")));
+
     }
 }
